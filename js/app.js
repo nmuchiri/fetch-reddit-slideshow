4dbb8ab80a99535f0c773805dd562e00e6b96302
nmuchiri <lonesomebounty@gmail.com>
2020-10-11 22:41:29 -0400

    commit 2 of the day

--- js/app.js
+++ js/app.js
@@ -40,53 +40,72 @@ document.addEventListener("DOMContentLoaded", ()=>{
     }
  
     
-   
+   let pics=[]
+   let i
 
     const addCatPhoto= () => {
         
         let slideShow=document.querySelector("#slideShow")
-        let pic = document.createElement('img')
+        let pic0 = document.createElement('img')
         // console.log(pic)
-        pic.classList.add("mySlides")
-        pic.setAttribute('src', cats[i].data.url)
-        console.log(pic)
-        slideShow.appendChild(pic)
-
+        // pic.classList.add("mySlides")
         cats.forEach(element => console.log(element))
+    
+            // console.log(cats[i].data.thumbnail)
+             
+        pic0.setAttribute('src', cats[0].data.thumbnail)
+        console.log(pic0)
+        slideShow.appendChild(pic0)
 
-        photo0= cats[2]
-        console.log(photo0)
+        let pic1 = document.createElement('img')
+        pic1.setAttribute('src', cats[1].data.thumbnail)
+        slideShow.appendChild(pic1)
+    
+        // pic.setAttribute('src', cats[i].data.thumbnail)
+        // console.log(pic)
+        // slideShow.appendChild(pic)
 
     
-   createSlideShow()
+//    createSlideShow()
 }
 
-
-let slideIndex=1
-createSlideShow(slideIndex)
-function createSlideShow(n){
-    let slides = document.querySelectorAll(".mySlides")
-    // console.log(slides)
-    for (i = 0; i < slides.length; i++) {
-    // console.log(slides[i])
-  
-    slides[i].style.display = "block" 
-   
+function createSlideShow(){
+    addCatPhoto()
+    document.slide.src= pics[i]
+    if(i < pics.length-1){
+        i++
+    }else{
+        i=0
     }
-    // if (n > slides.length) {
-    //     slideIndex = 1 }
-    //     console.log(slideIndex)
-    // if (n < 1) {slideIndex = slides.length}
-    // console.log(slides.length)
-    // slides[slideIndex-1].style.display = "block"
-    // console.log(slides[slideIndex-1])
+}
+setTimeout("createSlideShow()", 3000)
+
+
+// let slideIndex=1
+// createSlideShow(slideIndex)
+// function createSlideShow(n){
+//     let slides = document.querySelectorAll(".mySlides")
+//     console.log(slides)
+//     for (i = 0; i < slides.length; i++) {
+//     // console.log(slides[i])
+  
+//     slides[i].style.display = "block" 
+   
+//     }
+//     if (n > slides.length) {
+//         slideIndex = 1 }
+//         console.log(slideIndex)
+//     if (n < 1) {slideIndex = slides.length}
+//     console.log(slides.length)
+//     slides[slideIndex-1].style.display = "block"
+//     console.log(slides[slideIndex-1])
     
-} 
+// } 
 
      
 
-function plusSlides(n) {
-    createSlideShow(slideIndex += n);
+function plusSlides() {
+    createSlideShow
   }
   plusSlides()
   
